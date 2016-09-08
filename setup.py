f05e21c8bcfd85bd6d1f8553dbb377f268030f8e
Nicholas Whyte <nick@nickwhyte.com>
2016-09-08 15:36:15 +1000

    Flake8

--- setup.py
+++ setup.py
@@ -1,43 +1,42 @@
 import os
 from setuptools import setup
 
-
 def get_version():
     version_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         'VERSION')
     v = open(version_path).read()
     if type(v) == str:
-        return v.strip()
+            return v.strip()
     return v.decode('UTF-8').strip()
 
 readme_path = os.path.join(os.path.dirname(
-  os.path.abspath(__file__)),
-  'README.rst',
+    os.path.abspath(__file__)),
+    'README.rst',
 )
 long_description = open(readme_path).read()
 
 try:
-    version = get_version()
+        version = get_version()
 except Exception as e:
-    version = '0.0.0-dev'
+        version = '0.0.0-dev'
 
 setup(
-  name='marshmallow-sqlalchemy-referential',
-  version=version,
-  py_modules=['marshmallow_sqlalchemy_referential'],
-  author="Nick Whyte",
-  author_email='nick@nickwhyte.com',
-  description="A marshmallow-sqlalchemy field allows referential CRUD on "
-              "relational fields.",
-  long_description=long_description,
-  url='https://github.com/twopicode/marshmallow-sqlalchemy-referential',
-  zip_safe=False,
-  install_requires=[
+    name='marshmallow-sqlalchemy-referential',
+    version=version,
+    py_modules=['marshmallow_sqlalchemy_referential'],
+    author="Nick Whyte",
+    author_email='nick@nickwhyte.com',
+    description="A marshmallow-sqlalchemy field allows referential CRUD on "
+                "relational fields.",
+    long_description=long_description,
+    url='https://github.com/twopicode/marshmallow-sqlalchemy-referential',
+    zip_safe=False,
+    install_requires=[
         "marshmallow-sqlalchemy",
-  ],
-  classifiers=[
-    'Intended Audience :: Developers',
-    'Programming Language :: Python',
-  ],
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ],
 )
