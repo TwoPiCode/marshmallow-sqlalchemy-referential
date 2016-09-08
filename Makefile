ca874428e538ac89a315b9f6865a25999a8fd491
Nicholas Whyte <nick@nickwhyte.com>
2016-09-08 15:33:46 +1000

    Travis-ify

---
+++ Makefile
@@ -0,0 +1,7 @@
+test:
+	flake8
+	python setup.py test	
+
+install_precommit: 
+	echo "#!/bin/sh\nmake test" > .git/hooks/pre-commit;
+	chmod +x .git/hooks/pre-commit;
--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 import os
 from setuptools import setup
 
+
+def get_version():
+    version_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'VERSION')
+    v = open(version_path).read()
+    if type(v) == str:
+        return v.strip()
+    return v.decode('UTF-8').strip()
+
 readme_path = os.path.join(os.path.dirname(
   os.path.abspath(__file__)),
   'README.rst',
 )
 long_description = open(readme_path).read()
-version_path = os.path.join(os.path.dirname(
-  os.path.abspath(__file__)),
-  'VERSION',
-)
-version = open(version_path).read()
+
+try:
+    version = get_version()
+except Exception as e:
+    version = '0.0.0-dev'
 
 setup(
   name='marshmallow-sqlalchemy-referential',
@@ -24,7 +34,6 @@ setup(
   url='https://github.com/twopicode/marshmallow-sqlalchemy-referential',
   zip_safe=False,
   install_requires=[
-        "marshmallow",
         "marshmallow-sqlalchemy",
   ],
   classifiers=[
